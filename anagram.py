fb641367224e53eec87a5941c033886da97d8647
Jeffrey Shen <sunnyjeff0606@gmail.com>
2024-06-20 14:55:09 -0400

    v1

--- anagram.py
+++ anagram.py
@@ -1,20 +1,30 @@
 from trie import Trie
 from board import anaBoard
-def main():
-    #creates Trie
-    trie = Trie()
 
+trie = Trie()
+def maketrie():
+    #creates Trie
     words = open("newdict.txt", "r")
     for word in words:
         trie.insert(word.strip().lower())
     print("created trie succesfully")
 
+def solver():
     letters = input("insert board ")
     board = anaBoard()
     board.makeboard(letters)
 
     print(sorted(board.getall(trie), key=len, reverse=True))
 
+def start():
+    maketrie()
+    while True:
+        action = input("Type R to resume, Q to quit ")
+        if action == "Q":
+            return
+        elif action == "R":
+            solver()
+        else:
+            print("Invalid input")
 
-
-main()
+start()
